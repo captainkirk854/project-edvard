72b6641a71b73d959356fcecafa697869f755f03
captainkirk854 <patrickfraioli1@hotmail.com>
2016-05-27 23:21:18 +0100

    CSV file facility; DataTable Extension methods

--- Helpers/Data.cs
+++ Helpers/Data.cs
@@ -2,16 +2,19 @@
 {
     using System;
     using System.Data;
+    using System.IO;
+    using System.Diagnostics;
+    using System.Threading;
 
     public static class Data
     {
         /// <summary>
         /// Display contents of a DataTable type
         /// </summary>
-        /// <param name="table"></param>
+        /// <param name="iTable"></param>
         /// <param name="ColumnSpacing"></param>
         /// <param name="ColumnSpecific"></param>
-        public static void DisplayDataTable(DataTable iTable, int ColumnSpacing, string ColumnSpecific)
+        public static void Display(this DataTable iTable, int ColumnSpacing, string ColumnSpecific)
         {
             // Initialise ..
             DataTable table = new DataTable();
@@ -49,7 +52,7 @@
                 table = iTable; // Copy entire DataTable as everything is to be seen ..
             }
 
-
+            // Get DataTable metrics ..
             int columnTotal = table.Columns.Count;
             int rowTotal = table.Rows.Count;
 
@@ -93,6 +96,78 @@
             // Construct Column Footer(s) ...
             for (int i = 0; i < columnTotal; i++) { Console.Write(outline); }
             Console.Write(Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Create a CSV file
+        /// </summary>
+        /// <param name="iTable"></param>
+        /// <param name="csvFilepath"></param>
+        public static void CreateCSV(this DataTable iTable, string csvFilepath)
+        {
+            // Initialise ..
+            const string comma = ",";
+            string Column = string.Empty;
+
+            // Get DataTable metrics ..
+            int columnTotal = iTable.Columns.Count;
+            int rowTotal = iTable.Rows.Count;
+
+            if (File.Exists(csvFilepath)) { File.Delete(csvFilepath); }
+            StreamWriter csv = File.CreateText(csvFilepath);
+
+            // Create delimited list of column names .. 
+            for (int columnIndex = 0; columnIndex < columnTotal; columnIndex++)
+            {
+                Column += iTable.Columns[columnIndex].ToString() + comma;
+            }
+            csv.WriteLine(Column);
+
+            // Create delimited list of row values ..
+            for (int rowIndex = 0; rowIndex < rowTotal; rowIndex++)
+            {
+                string DataRow = string.Empty;
+                for (int ColumnIndex = 0; ColumnIndex < columnTotal; ColumnIndex++)
+                {
+                    DataRow += iTable.Rows[rowIndex][ColumnIndex].ToString() + comma;
+                }
+                csv.WriteLine(DataRow);
+            }
+            csv.Close();
+
+            //Info ..
+            Console.WriteLine("CREATED {0}", csvFilepath);
+        }
+
+        /// <summary>
+        /// Simple File Printing facility
+        /// </summary>
+        /// <param name="filename"></param>
+        private static void SendToPrinter(string filename)
+        {
+            // Define the print job ..
+            ProcessStartInfo printjob = new ProcessStartInfo();
+            printjob.Verb = "PRINT";
+            printjob.FileName = @filename;
+            printjob.CreateNoWindow = true;
+            printjob.WindowStyle = ProcessWindowStyle.Hidden;
+
+            // Start ..
+            Process printProcess = new Process();
+            printProcess.StartInfo = printjob;
+            printProcess.Start();
+
+            long ticks = -1;
+            while (ticks != printProcess.TotalProcessorTime.Ticks)
+            {
+                ticks = printProcess.TotalProcessorTime.Ticks;
+                Thread.Sleep(1000);
+            }
+
+            if (!printProcess.CloseMainWindow())
+            {
+                printProcess.Kill();
+            }
         }
     }
 }
--- QuickRunner/Program.cs
+++ QuickRunner/Program.cs
@@ -8,41 +8,45 @@
     {
         static void Main(string[] args)
         {
-            // Point to project sample (not a resource as such) data ..
-            string EDCfg = GetProjectDirectory() + "\\Sample" + "\\ED01.binds";
-            string VACfg = GetProjectDirectory() + "\\Sample" + "\\VA01.vap";
-            const int KeyBindingsColumnWidth = 20;
-
             // Initialise ..
             DataTable KeyBindingsTable = new DataTable();
 
-            // Read Elite Dangerous and Voice Attack configuration(s) to get key bindings ..
+            const int KeyBindingsColumnWidth = 20;
+            const string CSVFileName = "EDVA_Consolidated_KeyBindings.csv";
+
+            // Point to project sample (not a resource as such) data ..
+            string EDCfg = GetProjectDirectory() + "\\Sample" + "\\ED01.binds";
+            string VACfg = GetProjectDirectory() + "\\Sample" + "\\VA01.vap";
+
+            // Path for DataTable Debug Csv ..
+            string DataTableDebugCSV = Environment.ExpandEnvironmentVariables("%UserProfile%");
+            DataTableDebugCSV += "\\Desktop";
+            DataTableDebugCSV += "\\" + CSVFileName;
+
+            // Read EliteDangerous and Voice Attack configuration(s) to get key bindings ..
             try
             {
+                // Combine DataTables from both applications ..
                 KeyBindingsTable = Game.KeyBindingsConfigReader.EliteDangerous(EDCfg);
                 KeyBindingsTable.Merge(Game.KeyBindingsConfigReader.VoiceAttack(VACfg));
                 Console.WriteLine("Config(s) Read");
                 PressIt();
 
-                // Display DataTable Contents ..
-                Data.DisplayDataTable(KeyBindingsTable, KeyBindingsColumnWidth, string.Empty);
-                PressIt();
-
-                Data.DisplayDataTable(KeyBindingsTable, KeyBindingsColumnWidth + 60, "KeyId");
-                PressIt();
-
-                Data.DisplayDataTable(KeyBindingsTable, KeyBindingsColumnWidth + 60, "ModifierKeyId");
+                // Debug DataTable Contents ..
+                KeyBindingsTable.Display(KeyBindingsColumnWidth, string.Empty);
+                KeyBindingsTable.CreateCSV(DataTableDebugCSV);
                 PressIt();
             }
             catch
             {
-                Console.WriteLine("Something went wrong ... we cry");
+                Console.WriteLine("Something went wrong ... we cry real tears ...");
                 PressIt();
+                throw;
             }
         }
 
         /// <summary>
-        /// Crude way of getting current project directory
+        /// Crude way of getting current Visual Studio project directory
         /// </summary>
         /// <returns></returns>
         private static string GetProjectDirectory()
